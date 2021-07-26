43ca5d9954fa67c2976bbbb054373077ed505362
Aleksandra Czajczynska <czajczynska.aleksandra@gmail.com>
2021-07-26 16:23:11 +0200

    add sass postcss browserslist css modules

--- src/index.js
+++ src/index.js
@@ -1,4 +1,9 @@
 import './styles/index.scss';
+import styles from './styles/module-styles.css';
+
+const h2 = document.querySelector('h2');
+h2.classList.add(styles.heading);
+
 const obj = {
   a: 'apple scrips',
   b: 'buttercream',
