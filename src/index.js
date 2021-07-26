3e1f3dc3f7b0a783d6824cc3532678376ec99e04
Aleksandra Czajczynska <czajczynska.aleksandra@gmail.com>
2021-07-26 15:36:11 +0200

    add js and scss styles

--- src/index.js
+++ src/index.js
@@ -1,4 +1,13 @@
-console.log('it does need work! ne changes');
+import './styles/index.scss';
+const obj = {
+  a: 'apple scrips',
+  b: 'buttercream',
+  c: {
+    cd: 'cheese danish',
+  },
+};
+
+console.log(obj?.c?.cd, obj?.f?.fe);
 
 if (module.hot) {
   module.hot.accept();
