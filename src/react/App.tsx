3a5398216c3765314108678a6c816fceb78d92d6
Aleksandra Czajczynska <czajczynska.aleksandra@gmail.com>
2021-07-27 15:36:56 +0200

    simple svelte config

--- src/react/App.tsx
+++ src/react/App.tsx
@@ -2,6 +2,28 @@ import React, { useState } from 'react';
 import roomPic from '../images/room.jpeg';
 import anatomy from '../images/anatomy.png';
 import statistics from '../images/statistics.svg';
+import styled from 'styled-components';
+
+const Container = styled.div`
+  display: flex;
+  flex-direction: column;
+
+  place-items: center;
+  background-color: midnightblue;
+  color: cornflowerblue;
+  font-family: sans-serif;
+  width: 900px;
+  padding: 40px;
+
+  h2 {
+    font-size: 64px;
+  }
+
+  a {
+    font-size: 28px;
+    color: aliceblue;
+  }
+`;
 
 const Typograpy: React.FC = ({ children, ...props }) => (
   <h2 {...props}>{children}</h2>
@@ -11,14 +33,17 @@ const App = () => {
   const [state, setState] = useState(false);
 
   return (
-    <div>
+    <Container>
       <Typograpy onClick={() => setState(!state)}>
         What is the state? : {state.toString()}
       </Typograpy>
+      <a href='/index.html'>Home</a>
+      <a href='/vue.html'>Go to React app</a>
+      <a href='/svelte.html'>Go to Svelte app</a>
       <img src={roomPic} alt='room' width='800' />
       <img src={anatomy} alt='human' />
       <img src={statistics} alt='statistics' width='500' />
-    </div>
+    </Container>
   );
 };
 
